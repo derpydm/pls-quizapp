a2884b13617c7fb98d67e91db30e146776591108
Sean Wong <derpydm@gmail.com>
2019-11-27 09:38:02 +0800

    Initial Commit

---
+++ QuizPLS Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizPLS Template
+//
+//  Created by Tinkertanker on 27/11/19.
+//  Copyright © 2019 Tinkertanker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
